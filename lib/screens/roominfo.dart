44d6f394507fd866f97f11591469b990837b51ea
ItchyBeth <42030208+ItchyBeth@users.noreply.github.com>
2020-05-17 23:57:09 +0700

    Add room info

---
+++ lib/screens/roominfo.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+
+class room extends StatefulWidget {
+  @override
+  roomstate createState() => roomstate();
+}
+
+class roomstate extends State<room> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        padding: EdgeInsets.only(top: 30),
+        
+          child: Column(
+            children: <Widget>[
+              Container(
+                  child: Align(
+                alignment: Alignment.topLeft,
+                child: IconButton(
+                  icon: Icon(
+                    Icons.arrow_back,
+                    size: 50,
+                  ),
+                  color: Colors.orange,
+                  onPressed: () {},
+                ),
+              )),
+
+
+
+
+
+              Container(
+                alignment: Alignment.center,
+                
+                child: Text(
+                  "1st Floor",
+                  style: TextStyle(fontSize: 25, color: Colors.orange, fontWeight: FontWeight.bold
+                ),
+              ),
+              ),
+              Container(),
+            ],
+          ),
+        
+      ),
+    );
+  }
+}
