2b195cb47f78f76009ad4f60be3fd3bda75c625b
pen <Dummypyp@gmail.com>
2020-05-18 00:43:24 +0700

    d

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -6,21 +6,36 @@ class hpage extends StatefulWidget {
   hpagestate createState() => hpagestate();
 }
 
+
 class hpagestate extends State<hpage> {
+  sampleFunction(){
+    print('Button Clicked');
+  }
    @override
     Widget build(BuildContext context) {
-      return Scaffold(
-        body: Center(
-          child: RaisedButton(
-            onPressed: () => {},
-            color: Colors.orange,
-            child: Text(
-              'MAP',
-              style: TextStyle(color: Colors.white),
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child : Center(
+          child: 
+          Container(
+            width: 180.0,
+            height: 60.0,
+            child : new RaisedButton(
+                    onPressed: () => {},
+                    color: Colors.orange,
+                    child: new Text(
+                            "MAP",
+                            style: new TextStyle(
+                            fontSize: 30.0,
+                            color: Colors.white,),    
+                    ),
+                    ),
             ),
+            
           ),
-          ),
-        
+        ),
+      ),
     );
-    }
+  }
 }
