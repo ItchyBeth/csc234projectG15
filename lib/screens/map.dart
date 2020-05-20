2913d09dc7b200fd164e034a6bd6866f2ea76ce3
ppcyel <42583572+ppcyel@users.noreply.github.com>
2020-05-20 23:10:03 +0700

    Update map.dart

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -13,62 +13,103 @@ class searchstate extends State<search> {
       backgroundColor: Colors.white,
       body: Container(
         padding: EdgeInsets.only(top: 30),
-        
-          child: Column(
-            children: <Widget>[
-              Container(
-                  child: Align(
+        child: Column(
+          children: <Widget>[
+            Container(
+                child: Align(
+              alignment: Alignment.topLeft,
+              child: IconButton(
+                icon: Icon(
+                  Icons.arrow_back_ios,
+                  size: 50,
+                ),
+                color: Colors.orange,
+                onPressed: () {},
+              ),
+            )),
+            Container(
+              margin: EdgeInsets.only(left: 30.0, right: 30.0, top: 30.0),
+              child: new TextField(
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.symmetric(vertical: 15.0),
+                  hintText: "Search",
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.orange,
+                  ),
+                  enabledBorder: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(80)),
+                    borderSide: BorderSide(color: Colors.orange, width: 2),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 270.0,
+              child: Image.asset(
+                'ass/img/map.png',
+                width: 350,
+                height: 350,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 75.0, right: 30.0, top: 10.0),
+              child: Align(
                 alignment: Alignment.topLeft,
-                child: IconButton(
-                  icon: Icon(
-                    Icons.arrow_back,
-                    size: 50,
-                  ),
-                  color: Colors.orange,
-                  onPressed: () {},
-                ),
-              )),
-
-
-
-
-
-              Container(
-                
-                margin: EdgeInsets.only(left: 30.0, right: 30.0,top: 30.0),
-                child: new TextField(
-                
-                  decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.symmetric(vertical: 15.0),
-                    hintText: "Search",
-                    prefixIcon: Icon(
-                      Icons.search,
-                      color: Colors.orange,
-                    ),
-                    enabledBorder: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(80)),
-                      borderSide: BorderSide(color: Colors.orange, width: 2),
-                         
-                    ),
-                  ),
+                child: Text(
+                  'Living Room ',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30),
                 ),
               ),
-              Container(
-               height: 270.0,
-                child: Image.asset(
-          'ass/img/map.png',
-          width: 350,
-          height: 350,
+            ),
+            
+            Container(
+              margin: EdgeInsets.only(left: 75.0, right: 50.0, top: 10.0),
+              child: Column(
+                children: [
+                  
+                  Text(
+                    '_______________________',
+                    
+                    style: TextStyle(
+                        color: Colors.orange,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25),
+                  ),
+                  Row(children: [
+                    Icon(
+                      Icons.access_time,
+                      color: Colors.black,
+                      size: 25.0,
+                      semanticLabel: 'Text to announce in accessibility modes',
+                    ),
+                    Text(
+                      '  Open 9AM - 17AM',
+                      style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),
+                    )
+                  ]),
+                  Row(children: [
+                    Icon(
+                      Icons.place,
+                      color: Colors.black,
+                      size: 25.0,
+                      semanticLabel: 'Text to announce in accessibility modes',
+                    ),
+                    Text(
+                      '  At 1st Floor',
+                      style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),
+                    )
+                  ]),
+                  Row(children: []),
+                ],
+              ),
+            ),
+          ],
         ),
-              ),
-            ],
-          ),
-        
       ),
-      
     );
   }
 }
-
-
-
