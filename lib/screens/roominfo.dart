fdb0bb49a6e75349bf64785bb87224f07fc1ff2a
ppcyel <42583572+ppcyel@users.noreply.github.com>
2020-05-21 01:27:52 +0700

    Update roominfo.dart

--- lib/screens/roominfo.dart
+++ lib/screens/roominfo.dart
@@ -2,131 +2,127 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/screens/homepage.dart';
 
-
-class room extends StatefulWidget {
+class search extends StatefulWidget {
   @override
-  roomstate createState() => roomstate();
+  searchstate createState() => searchstate();
 }
 
-class roomstate extends State<room> {
+class searchstate extends State<search> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Container(
         padding: EdgeInsets.only(top: 30),
-        
-          child: Column(
-            children: <Widget>[
-              Container(
-                  child: Align(
-                alignment: Alignment.topLeft,
-                child: IconButton(
-                  icon: Icon(
-                    Icons.arrow_back_ios,
-                    size: 40,
+        child: Column(
+          children: <Widget>[
+            Container(
+                child: Align(
+              alignment: Alignment.topLeft,
+              child: IconButton(
+                icon: Icon(
+                  Icons.arrow_back_ios,
+                  size: 50,
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => hpage()),
+                  );
+                },
+                color: Colors.orange,
+              ),
+            )),
+            Container(
+              margin: EdgeInsets.only(left: 30.0, right: 30.0, top: 30.0),
+              child: new TextField(
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.symmetric(vertical: 15.0),
+                  hintText: "Search",
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.orange,
                   ),
-                  color: Colors.orange,
-                  onPressed: () {Navigator.push(context,MaterialPageRoute(builder: (context) => hpage()),);},
+                  enabledBorder: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(80)),
+                    borderSide: BorderSide(color: Colors.orange, width: 2),
+                  ),
                 ),
-              )),
-
-              Container(
-                alignment: Alignment.center,
-                margin: EdgeInsets.only(top: 20.0),
-                
+              ),
+            ),
+            Container(
+              height: 270.0,
+              child: Image.asset(
+                'ass/img/map.png',
+                width: 350,
+                height: 350,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 75.0, right: 30.0, top: 10.0),
+              child: Align(
+                alignment: Alignment.topLeft,
                 child: Text(
-                  "1st Floor",
-                  style: TextStyle(fontSize: 25, color: Colors.orange, fontWeight: FontWeight.bold
+                  'Living Room ',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30),
                 ),
               ),
-              ),
-
-
-              Container(
-                alignment: AlignmentDirectional.center,
-                margin: EdgeInsets.only(bottom: 20.0),
-
-                child: Image.asset(
-                  'ass/img/map.png',
-                  width: 350,
-                ),
-              ),
-
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-
-                  children: const <Widget>[
-                    IconButton(
-                      icon: Icon(Icons.home), color: Colors.orange, iconSize: 40.0,
-                      onPressed: null),
-                    IconButton(
-                      icon: Icon(Icons.fastfood), color: Colors.orange, iconSize: 40.0,
-                      onPressed: null),
-                    IconButton(
-                      icon: Icon(Icons.unfold_more), color: Colors.orange, iconSize: 40.0,
-                      onPressed: null),
-                    IconButton(
-                      icon: Icon(Icons.wc), color: Colors.orange, iconSize: 40.0,
-                      onPressed: null),
-
-                  ],
-                ),
-      
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-
-
-                  children: const <Widget>[
-                    Text(
-                      'Rooms', 
-                      style: TextStyle(fontSize: 20, color: Colors.orange, fontWeight: FontWeight.bold),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 75.0, right: 50.0, top: 10.0),
+              child: Column(
+                children: [
+                  Text(
+                    '_______________________',
+                    style: TextStyle(
+                        color: Colors.orange,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25),
+                  ),
+                  Row(children: [
+                    Icon(
+                      Icons.access_time,
+                      color: Colors.black,
+                      size: 25.0,
+                      semanticLabel: 'Text to announce in accessibility modes',
                     ),
                     Text(
-                      'Number', 
-                      style: TextStyle(fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
+                      '  Open 9AM - 7PM',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    )
+                  ]),
+                  Row(children: [
+                    Icon(
+                      Icons.place,
+                      color: Colors.black,
+                      size: 25.0,
+                      semanticLabel: 'Text to announce in accessibility modes',
                     ),
-                  ],
-                ),
+                    Text(
+                      '  At 1st Floor',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    )
+                  ]),
 
-                Column(
-                    children: <Widget>[
-                      RaisedButton(
-                        onPressed: (){},
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        textColor: Colors.black,
-                        padding: const EdgeInsets.all(0.0),
-                        child: Container(
-                          width: 300,
-                          padding: const EdgeInsets.only(left: 20, right: 20),
-                          child: Row(
-                            children: <Widget>[
-                            Text(
-                            'Cafe',
-                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                           Text(
-                            '1',
-                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                            ],
-                          ),
-                          
-                          
-                          
-                        ),
-                      )
-                    ],
-                ),
-            ],
-          ),
+
+
+        
+
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-
-      
-      
     );
   }
 }
